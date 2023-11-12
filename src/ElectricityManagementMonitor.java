905a22d1a9192fd9afea9baf699d8d09ee0742a1
Abdulraheem Bashaweeh <abashaweeh@stu.kau.edu.sa>
2023-11-12 21:50:31 +0300

    Created Main class. close #5

--- src/ElectricityManagementMonitor.java
+++ src/ElectricityManagementMonitor.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class ElectricityManagementMonitor {
+    private static ElectricityManagementMonitor instance;
 
+    private List<Building> buildings;
+
+    private ElectricityManagementMonitor() {
+        buildings = new ArrayList<>();
+    }
+
+    public static ElectricityManagementMonitor getInstance() {
+        if (instance == null) {
+            instance = new ElectricityManagementMonitor();
+        }
+        return instance;
+    }
 }
