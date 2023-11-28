3ea6c42fc139b956cbd27fbd206317a68450a62a
Abdulraheem Bashaweeh <abashaweeh@stu.kau.edu.sa>
2023-11-28 18:53:35 +0300

    Added a method to add a tax. close 19#

--- src/BillingDecorator.java
+++ src/BillingDecorator.java
@@ -1,2 +1,15 @@
-public class BillingDecorator {
+class BillingDecorator implements ElectricityComponent {
+    private ElectricityComponent component;
+
+    public BillingDecorator(ElectricityComponent component) {
+        this.component = component;
+    }
+
+    @Override
+    public double calculateTotalElectricityBill() {
+        // Add the 15% Tax.
+        double additionalCost = component.calculateTotalElectricityBill() * 0.15;
+        return component.calculateTotalElectricityBill() + additionalCost;
+    }
 }
+
