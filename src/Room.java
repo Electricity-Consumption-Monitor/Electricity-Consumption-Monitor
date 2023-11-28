ba93cc82cebd8358d0e7703dcdc104da8c177d41
Abdulrahman Nahfawi <113578367+abdoabdo2@users.noreply.github.com>
2023-11-28 18:43:59 +0300

    added room methods. close #17

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,16 @@
-public class Room {
+import java.util.ArrayList;
+import java.util.List;
+
+class Room  {
+    private List<Device> devices;
+
+    public Room() {
+        devices = new ArrayList<>();
+    }
+
+    public void addDevice(Device device) {
+        devices.add(device);
+    }
+
+    
 }
