07809f72f6e0be261f42026263400b6b0128d7e1
Abdulraheem Bashaweeh <abashaweeh@stu.kau.edu.sa>
2023-11-28 18:55:33 +0300

    Implementing decorator on Building. close #20

--- src/Building.java
+++ src/Building.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 // Concrete Component
-class Building  {
+class Building implements ElectricityComponent {
     private String name;
     private List<Room> rooms;
 
@@ -15,7 +15,15 @@ class Building  {
     public void addRoom(Room room) {
         rooms.add(room);
     }
-    
+
+    @Override
+    public double calculateTotalElectricityBill() {
+        double totalElectricityBill = 0.0;
+        for (Room room : rooms) {
+            totalElectricityBill += room.calculateTotalElectricityBill();
+        }
+        return totalElectricityBill * 0.18;
+    }
 
     public String getName() {
         return name;
