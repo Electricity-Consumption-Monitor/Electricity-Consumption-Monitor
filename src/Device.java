762b86f82b3cd83a3964f70665b531c196445f3a
Abdulrahman Nahfawi <113578367+abdoabdo2@users.noreply.github.com>
2023-11-28 18:36:14 +0300

    added device method. close #15

--- src/Device.java
+++ src/Device.java
@@ -1,2 +1,14 @@
-public class Device {
+import java.util.Scanner;
+
+class Device {
+    private String name;
+    private double hoursOfWork;
+    private double wattsConsumed;
+
+    public Device(String name, double hoursOfWork, double wattsConsumed) {
+        this.name = name;
+        this.hoursOfWork = hoursOfWork;
+        this.wattsConsumed = wattsConsumed;
+    }
+
 }
