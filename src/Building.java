ab5f5cfd3df109df85079a620455c2812276984e
Abdulrahman Nahfawi <113578367+abdoabdo2@users.noreply.github.com>
2023-11-28 18:32:49 +0300

    added class building methods. close #14

--- src/Building.java
+++ src/Building.java
@@ -1,2 +1,23 @@
-public class Building {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// Concrete Component
+class Building  {
+    private String name;
+    private List<Room> rooms;
+
+    public Building(String name) {
+        this.name = name;
+        rooms = new ArrayList<>();
+    }
+
+    public void addRoom(Room room) {
+        rooms.add(room);
+    }
+    
+
+    public String getName() {
+        return name;
+    }
 }
